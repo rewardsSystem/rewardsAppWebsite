dd00c2d2ce427274735c80ec10898ccf2969a603
Ovidio R. Calvet <ovidio@calvetsolutions.com>
2015-06-03 18:39:38 -0400

    Added a stub spec

---
+++ specs/UserController-spec.js
@@ -0,0 +1,7 @@
+var userController = require("../api/controllers/UserController");
+
+describe("UserController", function() {
+    it("should get one user when an id is set in the params", function() {
+        expect(true).toBe(true);
+    });
+});;
