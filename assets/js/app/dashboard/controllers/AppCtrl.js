1419eba314c7d0324845cf3a54f8e6a66e0e77c7
Ovidio R. Calvet <ovidio@calvetsolutions.com>
2015-06-03 22:16:36 -0400

    Added a menu and a card

--- assets/js/app/dashboard/controllers/AppCtrl.js
+++ assets/js/app/dashboard/controllers/AppCtrl.js
@@ -3,8 +3,18 @@
     'use strict';
 
     ng.module('dashboardApp')
-    .controller('AppCtrl', ['$scope', function($scope) {
-        $scope.test="Angular working...";
-    }]);
+        .controller('AppCtrl', ['$scope', 'cardService', '$mdToast', function($scope, cardService, $mdToast) {
+            
+            $scope.cards = []; 
+
+            cardService.getCards()
+                .success(function(cards){
+                    $scope.cards = cards;
+                })
+                .error(function(error){
+                    console.log("Error ", error);
+                    $mdToast.show($mdToast.simple().position("bottom right").content("Error: " + error));
+                });
+        }]);
 
 })(angular)
